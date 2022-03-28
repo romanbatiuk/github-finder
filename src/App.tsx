8baef89de5e7f2e43b3dc6529461c69df2cdc282
romanbatiuk <roman.batiuk87@gmail.com>
2022-03-28 21:54:25 +0300

    install tailwindcss and daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App(): JSX.Element {
 	return (
-		<div className="App">
-			<h1>GitHub Finder!!</h1>
+		<div className="App bg-purple-500">
+			<h1 className="text-3xl font-bold underline ">GitHub Finder!!</h1>
+			<button className="btn btn-primary">Click</button>
 		</div>
 	)
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	daisyui: {
+		themes: ['dark'],
+	},
+	plugins: [require('daisyui')],
+}
